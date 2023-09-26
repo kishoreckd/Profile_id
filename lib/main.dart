fad9d1f6f9e0be6b66d93d705993fb57f3cdc695
Kishore Kumar <kishorekumarcdckap@gmail.com>
2023-09-26 16:46:03 +0530

    The current level as been increased and decreased dynamically with the help of stateful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MaterialApp(home: NinjaCard()));
 
-class NinjaCard extends StatelessWidget {
+class NinjaCard extends StatefulWidget {
   const NinjaCard({super.key});
+
+  @override
+  State<NinjaCard> createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+  int profileLevel = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -14,28 +21,36 @@ class NinjaCard extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.grey[850],
           elevation: 0.0),
-      body: const Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            profileLevel += 1;
+          });
+        },
+        backgroundColor: Colors.grey[800],
+        child: const Icon(Icons.add),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Center(
+            const Center(
                 child: CircleAvatar(
               backgroundImage: AssetImage('assets/avatar.png'),
               backgroundColor: Colors.white,
               radius: 40.0,
-            )
-            ),
-            Divider(
+            )),
+            const Divider(
               height: 60.0,
               color: Colors.blue,
             ),
-            Text(
+            const Text(
               'Name',
               style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
             ),
-            SizedBox(height: 10.0),
-            Text(
+            const SizedBox(height: 10.0),
+            const Text(
               'Kishore',
               style: TextStyle(
                   color: Colors.amber,
@@ -43,22 +58,22 @@ class NinjaCard extends StatelessWidget {
                   letterSpacing: 2.0,
                   fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 30.0),
-            Text(
+            const SizedBox(height: 30.0),
+            const Text(
               ' Current Level',
               style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
             ),
-            SizedBox(height: 10.0),
+            const SizedBox(height: 10.0),
             Text(
-              '3',
-              style: TextStyle(
+              '$profileLevel',
+              style: const TextStyle(
                   color: Colors.amber,
                   fontSize: 28.0,
                   letterSpacing: 2.0,
                   fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 30.0),
-            Row(
+            const SizedBox(height: 30.0),
+            const Row(
               children: [
                 Icon(Icons.email, color: Colors.white),
                 SizedBox(
@@ -73,10 +88,99 @@ class NinjaCard extends StatelessWidget {
                   ),
                 )
               ],
-            )
+            ),
+            TextButton(
+                onPressed: () {
+                  setState(() {
+                    profileLevel -= 1;
+                  });
+                },
+                child: const Icon(Icons.remove)),
           ],
         ),
       ),
     );
   }
 }
+
+
+
+// // StatelessWidget
+// class NinjaCard extends StatelessWidget {
+//   const NinjaCard({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.grey[900],
+//       appBar: AppBar(
+//           title: const Text('Profile Id card'),
+//           centerTitle: true,
+//           backgroundColor: Colors.grey[850],
+//           elevation: 0.0),
+//       body: const Padding(
+//         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             Center(
+//                 child: CircleAvatar(
+//               backgroundImage: AssetImage('assets/avatar.png'),
+//               backgroundColor: Colors.white,
+//               radius: 40.0,
+//             )
+//             ),
+//             Divider(
+//               height: 60.0,
+//               color: Colors.blue,
+//             ),
+//             Text(
+//               'Name',
+//               style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
+//             ),
+//             SizedBox(height: 10.0),
+//             Text(
+//               'Kishore',
+//               style: TextStyle(
+//                   color: Colors.amber,
+//                   fontSize: 28.0,
+//                   letterSpacing: 2.0,
+//                   fontWeight: FontWeight.bold),
+//             ),
+//             SizedBox(height: 30.0),
+//             Text(
+//               ' Current Level',
+//               style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
+//             ),
+//             SizedBox(height: 10.0),
+//             Text(
+//               '3',
+//               style: TextStyle(
+//                   color: Colors.amber,
+//                   fontSize: 28.0,
+//                   letterSpacing: 2.0,
+//                   fontWeight: FontWeight.bold),
+//             ),
+//             SizedBox(height: 30.0),
+//             Row(
+//               children: [
+//                 Icon(Icons.email, color: Colors.white),
+//                 SizedBox(
+//                   width: 10.0,
+//                 ),
+//                 Text(
+//                   'kishorekumarcdckap@gmail.com',
+//                   style: TextStyle(
+//                     color: Colors.white,
+//                     fontSize: 18.0,
+//                     letterSpacing: 1.0,
+//                   ),
+//                 )
+//               ],
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
